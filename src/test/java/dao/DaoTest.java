883ca02586c39f5f769bd175258d480483b6d8de
guaragames <guaralauncher@gmail.com>
2025-10-06 11:04:56 -0300

    Versao 0.1.

---
+++ src/test/java/dao/DaoTest.java
@@ -0,0 +1,21 @@
+
+package dao;
+
+
+
+/**
+ *
+ * @author Usuário
+ */
+public class DaoTest {
+    
+    public DaoTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testInserir() {
+        
+        
+    }
+    
+}
