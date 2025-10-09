b1b1ca75fe24a50fc268a185ddd3d17b75b2851d
guaragames <guaralauncher@gmail.com>
2025-10-09 15:39:08 -0300

    Versao 0.2.

--- src/test/java/dao/DaoTest.java
+++ src/test/java/dao/DaoTest.java
@@ -1,6 +1,9 @@
 
 package dao;
 
+import java.util.List;
+import modelo.Veiculo;
+
 
 
 /**
@@ -12,10 +15,26 @@ public class DaoTest {
     public DaoTest() {
     }
 
-    @org.junit.jupiter.api.Test
-    public void testInserir() {
-        
-        
-    }
+//    @org.junit.jupiter.api.Test
+//    public void testInserir() {
+//        Veiculo v = new Veiculo("DER9865", "Ford", "Mustang");
+//        Dao<Veiculo> dao = new Dao(Veiculo.class); 
+//        Veiculo aux = dao.buscarPorChave("placa", "DER9865");
+//        if ( aux == null){  // não existe
+//            dao.inserir(v);  
+//        }
+//        else{
+//            System.out.println("Já existe");
+//        }      
+//    }
     
+//    @org.junit.jupiter.api.Test
+//    public void testListar(){
+//        List<Veiculo> lista;
+//        Dao<Veiculo> dao = new Dao(Veiculo.class);
+//        lista = dao.listarTodos(); 
+//        for (Veiculo v : lista){
+//            System.out.println(v.getPlaca());
+//        }
+//    }
 }
